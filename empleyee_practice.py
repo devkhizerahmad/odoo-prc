28cefa0732e4fac4477e1742457f12194c28fd80
Khizar Ahmed <devkhizerahmad@gmail.com>
2026-02-04 21:13:45 +0500

    updated

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -26,3 +26,4 @@ sorted_phones[0].show_details()  # Sabse sasta phone dikhaye
 sorted_phones[-1].show_details()  # Sabse mehnga phone dikhaye
 sorted_phones[2].show_details()  # Beech ka phone dikhaye
 my_phone3.show_details()
+my_phone4.show_details()
