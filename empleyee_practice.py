b15c167c190e156af3729edf20c2d8f716bf763f
Khizar Ahmed <devkhizerahmad@gmail.com>
2026-02-05 20:49:22 +0500

    my_phone5 = Mobile("Realme", 20000)

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -17,7 +17,6 @@ my_phone2 = Mobile("Iphone", 100000)
 my_phone3 = Mobile("OnePlus", 60000)
 my_phone4 = Mobile("Xiaomi", 30000)
 my_phone5 = Mobile("Realme", 20000)
-my
 sorted_phones = sorted([my_phone, my_phone2, my_phone3, my_phone4, my_phone5], key=lambda x: x.price)
 print("Phones sorted by price:", [phone.brand for phone in sorted_phones])
 # 5. Function call karo
