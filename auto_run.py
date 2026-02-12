3ec404590bd3cea5085963db99bfb13e3df23a21
Khizar Ahmed <devkhizerahmad@gmail.com>
2026-02-12 19:52:58 +0500

    updated

--- auto_run.py
+++ auto_run.py
@@ -29,6 +29,6 @@ class Handler(FileSystemEventHandler):
             subprocess.run(["python", self.file_to_watch])
 
 if __name__ == "__main__":
-    file_name = "empleyee_practice.py"  # apni main file 
+    file_name = "empleyee_practice.py"  # apni main 
     w = Watcher(file_name)
     w.run()
