c1a0c004c6fb362d13b66ad8314ed92452ccba65
Khizar Ahmed <devkhizerahmad@gmail.com>
2026-02-20 16:19:57 +0500

    updated

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -302,3 +302,4 @@ class Bank:
         print(f"Transaction {i+1}: Balance is {bank.balance}")
         print(f"Transaction {i+1}: Insufficient funds for withdrawal. Current balance: {bank.balance}")
     case 4: Final status
+    nzdc sa
