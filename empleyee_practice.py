046c04974100c6066afa5192b7910cbeb131634f
dev_khizer <devkhizerahmad@gmail.com>
2026-02-25 16:18:55 +0500

    Fix Mobile class definition by removing junk characters
    Removed invalid characters from the Mobile class definition.

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -1,6 +1,5 @@
 # # 1. Ek class banao jiska naam ho "Mobile"
 # class Mobile:
-jjmokjjjkjjjjjjjjjjjjj
 #     # 2. Ek constructor (__init__) banao jo 'brand' aur 'price' le
 #     def __init__(self, brand, price):
 #         self.brand = brand
