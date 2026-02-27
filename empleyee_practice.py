7895e118715afa688d765c8dd9205dfc0f244f07
dev_khizer <devkhizerahmad@gmail.com>
2026-02-27 16:49:10 +0500

    Fix typo in empleyee_practice.py
    Corrected a typo in the code.

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -5,7 +5,7 @@ asdhj# # 1. Ek class banao jiska naam ho "Mobile"
 #         self.brand = brand jj
 #         self.price = price
 #         print(f"Mobasdile createsdd with brand {self.brand} and price {self.price}")
-    $$324sdvazc zxadcsaasdcsa
+    $$324sdvazc zxadcsaasdcsaavav
 #     # 3. Ek function banao jo print kare "Ye mobile [brand] ka hai"
 #     def show_details(self):asd
 #         print(sadf"modile brand {self.brand} with the price {self.price} ")
