be7460847aba1a2f5646eeff0b44b304b15ca497
dev_khizer <devkhizerahmad@gmail.com>
2026-02-27 16:50:14 +0500

    Fix syntax errors in empleyee_practice.py

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -5,7 +5,7 @@ asdhj# # 1. Ek class banao jiska naam ho "Mobile"
 #         self.brand = brand jj
 #         self.price = price
 #         print(f"Mobasdile createsdd with brand {self.brand} and price {self.price}")
-    $$324sdvazc zxadcsaasdcsaavavasd
+    $$324sdvazc zxadcsaasdcsaav
 #     # 3. Ek function banao jo print kare "Ye mobile [brand] ka hai"
 #     def show_details(self):asd
 #         print(sadf"modile brand {self.brand} with the price {self.price} ")
