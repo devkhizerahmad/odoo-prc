97cf140908631c172af957e04e2e35a3f8c3a331
dev_khizer <devkhizerahmad@gmail.com>
2026-03-05 17:22:20 +0500

    Update empleyee_practice.py

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -5,7 +5,7 @@ asdhj# # 1. Ek class banao jiska naam ho "Mobile"
 #         self.brand = brand jj
 
 kj,mbkasdcasdc
-#         self.price = price
+#         self.price = pricesadcsacd as
 #         print(f"Mobasdile creatqasdcasdwefesdd with brand nm{self.brand} and price {self.price}")
     $$324sdvazc zxadcsaasdcsaav qwefqwwqefqwjvgukhjvb
 #     # 3. Ek function banao jo print kare "Ye mobeqewile [brand] ka hai"
