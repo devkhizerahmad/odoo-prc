a2c5c266a6c7f12e478beb5de06ec8f8dfebc243
dev_khizer <devkhizerahmad@gmail.com>
2026-03-10 16:32:46 +0500

    Update empleyee_practice.py

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -8,7 +8,7 @@ kj,mbkasdcasdcasdcaasd asdcasdca
 #         self.price = pricesadcsacd asasdcsd sadscas dadsadadsca  ma /
 console.log there is more then the optical andwec
 and the is nomre thenth esd s
-#         print(f"Mobasdile creatqasdcasdwefesdd with sacasdcbrand nm{self.brand} and price {self.price}")sadas d
+#         print(f"Mobasdile creatqasdcasdwefesdd with sacasdcbrand nm{self.brand} and price {swecwelf.price}")sadas d
     $$324sdvazc zxadcsaasdcsaav qwefqwwqefqwjvgukhjvbsadcsaasdcsd adscasdsd sadscsad
 #     # 3. Ek function banao jo print kare "Ye mobeqewile [brand] ka hai"sd cswecw 
 #     def show_details(self):asdasdcsadc
