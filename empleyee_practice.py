ba8833f70c3a91cc0247d5461bd0ac8c5b65fd11
dev_khizer <devkhizerahmad@gmail.com>
2026-03-11 16:12:22 +0500

    Update empleyee_practice.py

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -6,7 +6,7 @@ sd sasdhj# # 1. Ek class banao jiska naam ho "Mobile"
 sasadccasdcaasdcs
 kj,mbkasdcasdcasdcaasd asdcasdcasdvdsa
 #         self.price = pricesadcsacd asasdcsd sadscas dadsadadsca  ma /
-console.log there is more then the optical andwecadsvasdadsva
+console.log there is more then the optical andwecadsvasdadsvaadsvsa
 and the is nomre thenth esd sasdvsad
 #         print(f"Mobasdile creatqasdcasdwefesdd with sacasdcbranasdvad nm{self.brand} and price {swecwelf.price}")sadas d
     $$324sdvazc zxadcsaasdcsaav qwefqwwqefqwjvgukhjvbsadcsaasdcsd adscasdsd sadscsad
