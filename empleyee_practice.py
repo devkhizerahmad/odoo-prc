353e98ced7d0558ebd912b51d551d801bc5e0e57
dev_khizer <devkhizerahmad@gmail.com>
2026-03-19 15:22:41 +0500

    Update empleyee_practice.py

--- empleyee_practice.py
+++ empleyee_practice.py
@@ -4,7 +4,7 @@ sd sasdhj# # 1. Ek class banao jiska naam ho "Mobile"
 #     def __init__(self, brand, price):
 #         self.brand = brand jjsadasdcsaqweqw
 sasadccasdcaasdcsasdasdasadsasdasads
-kj,mbkasdcasdcasdcaasd asdcasdcasdvdsaasdvsaadssaadasadcwrwadssdfcasasdasdasd
+kj,mbkasdcasdcasdcaasd asdcasdcasdvdsaasdvsaadssaadasadcwrwadssdfcasasdasdasda
 #         self.price = pricesadcsacd asasdcsd sadscas daasdsadadasdsca  maasdasd  /werasdcasdaasd
 console.log there is more then the opticalsadasddsf andwecadsvadassaasdadsvaadadcsasvsaasdcsasadcasdawqef
 and the isda nomre thenth esd sasdvsadadsvasdasdasdfsadasdc
